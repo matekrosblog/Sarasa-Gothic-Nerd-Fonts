aef69b8ff70eec51e48992bf28bdac1f5fd2245f
jonz94 <jody16888@gmail.com>
2021-11-25 19:48:42 +0800

    🔄 refactor: prepare font patcher and source glyphs before patching

---
+++ download-source-glyphs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir -p src/glyphs
+
+glyphs=(
+  "FontAwesome.otf"
+  "NerdFontsSymbols 1000 EM Nerd Font Complete Blank.sfd"
+  "NerdFontsSymbols 2048 EM Nerd Font Complete Blank.sfd"
+  "Pomicons.otf"
+  "PowerlineExtraSymbols.otf"
+  "PowerlineSymbols.otf"
+  "Symbols Template 1000 em.ttf"
+  "Symbols Template 2048 em.ttf"
+  "Symbols-1000-em Nerd Font Complete.ttf"
+  "Symbols-2048-em Nerd Font Complete.ttf"
+  "Unicode_IEC_symbol_font.otf"
+  "devicons.ttf"
+  "font-awesome-extension.ttf"
+  "font-logos.ttf"
+  "materialdesignicons-webfont.ttf"
+  "octicons.ttf"
+  "original-source.otf"
+  "weathericons-regular-webfont.ttf"
+)
+
+upstream_src_glyphs_url="https://github.com/ryanoasis/nerd-fonts/blob/${NERD_FONTS_VERSION:-v2.1.0}/src/glyphs"
+
+for glyph in "${glyphs[@]}"; do
+  # replace all `whitespace` characters with `%20`
+  percent_encoded_uri="${upstream_src_glyphs_url}/${glyph//\ /%20}?raw=true"
+
+  curl -fsSL ${percent_encoded_uri} --output "src/glyphs/${glyph}"
+done
