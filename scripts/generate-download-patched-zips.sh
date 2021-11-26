0a97db763ce2254326a60d786f3c60a5c9391a4f
jonz94 <jody16888@gmail.com>
2021-11-27 04:48:22 +0800

    🎨 style: format coding style

--- scripts/generate-download-patched-zips.sh
+++ scripts/generate-download-patched-zips.sh
@@ -8,7 +8,6 @@ counter=0
 styles=("fixed" "fixed-slab" "mono" "mono-slab" "term" "term-slab" "gothic" "ui")
 orthographies=("cl" "hc" "j" "k" "sc" "tc")
 
-
 for style in ${styles[@]}; do
   for orthography in ${orthographies[@]}; do
     yq e -i ".jobs.job.steps[$counter].uses = \"actions/download-artifact@v2\"" $outputFile
